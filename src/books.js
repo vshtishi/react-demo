eef89568f939d1f4e77482a46cc595d30b20ff28
Vjola <vjolashtishi@gmail.com>
2021-09-21 16:17:16 +0200

    Separated books Data

---
+++ src/books.js
@@ -0,0 +1,38 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51Sjypj1F7L._AC_UL200_SR200,200_.jpg",
+    title: "No Longer Human",
+    author: "Osamu Dazai",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81WUAoL-wFL._AC_UL200_SR200,200_.jpg",
+    title: "Lord of the Flies",
+    author: "William Golding",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51cTFYeOAXS._AC_UL200_SR200,200_.jpg",
+    title: "The Handmaid's tale",
+    author: "Margaret Atwood",
+  },
+  {
+    id: 4,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71FTb9X6wsL._AC_UL160_SR160,160_.jpg",
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+  },
+  {
+    id: 5,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71j3JB9HHsL._AC_UL160_SR160,160_.jpg",
+    title: "The Scarlet Letter",
+    author: "Nathaniel Hawthorne",
+  },
+  {
+    id: 6,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81-tbD97VRL._AC_UL160_SR160,160_.jpg",
+    title: "The Crucible",
+    author: "Arthur Miller",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -3,57 +3,47 @@ import ReactDom from "react-dom";
 
 //CSS
 import "./index.css";
-//setup vars
-const books = [
-  {
-    img: "https://images-na.ssl-images-amazon.com/images/I/51Sjypj1F7L._AC_UL200_SR200,200_.jpg",
-    title: "No Longer Human",
-    author: "Osamu Dazai",
-  },
-  {
-    img: "https://images-na.ssl-images-amazon.com/images/I/81WUAoL-wFL._AC_UL200_SR200,200_.jpg",
-    title: "Lord of the Flies",
-    author: "William Golding",
-  },
-  {
-    img: "https://images-na.ssl-images-amazon.com/images/I/51cTFYeOAXS._AC_UL200_SR200,200_.jpg",
-    title: "The Handmaid's tale",
-    author: "Margaret Atwood",
-  },
-  {
-    img: "https://images-na.ssl-images-amazon.com/images/I/71FTb9X6wsL._AC_UL160_SR160,160_.jpg",
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-  },
-  {
-    img: "https://images-na.ssl-images-amazon.com/images/I/71j3JB9HHsL._AC_UL160_SR160,160_.jpg",
-    title: "The Scarlet Letter",
-    author: "Nathaniel Hawthorne",
-  },
-  {
-    img: "https://images-na.ssl-images-amazon.com/images/I/81-tbD97VRL._AC_UL160_SR160,160_.jpg",
-    title: "The Crucible",
-    author: "Arthur Miller",
-  },
-];
+import { books } from "./books";
 
 function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book book={book} />;
+        return <Book key={book.id} {...book} />;
       })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props.book;
+const Book = ({ img, title, author }) => {
+  const clickHandler = () => {
+    alert("test");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return (
-    <article className="book">
-      <img src={img} />
-      <h1>{title}</h1>
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
+      <img src={img} alt="" />
+      <h1
+        onClick={() => {
+          alert(title);
+        }}
+      >
+        {title}
+      </h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        Test
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        Test2
+      </button>
     </article>
   );
 };
