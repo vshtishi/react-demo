5fb17f8d87c8fa1636e6ed1443ba3411efe90d2c
Vjola <vjolashtishi@gmail.com>
2021-09-21 11:43:21 +0200

    Passed Props to Book Component

--- src/index.js
+++ src/index.js
@@ -4,24 +4,51 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 //setup vars
-const author = "Osamu Dazai";
-const title = "No Longer Human";
-const img = "https://images-na.ssl-images-amazon.com/images/I/51Sjypj1F7L._AC_UL200_SR200,200_.jpg"
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51Sjypj1F7L._AC_UL200_SR200,200_.jpg",
+    title: "No Longer Human",
+    author: "Osamu Dazai",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81WUAoL-wFL._AC_UL200_SR200,200_.jpg",
+    title: "Lord of the Flies",
+    author: "William Golding",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51cTFYeOAXS._AC_UL200_SR200,200_.jpg",
+    title: "The Handmaid's tale",
+    author: "Margaret Atwood",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71FTb9X6wsL._AC_UL160_SR160,160_.jpg",
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71j3JB9HHsL._AC_UL160_SR160,160_.jpg",
+    title: "The Scarlet Letter",
+    author: "Nathaniel Hawthorne",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81-tbD97VRL._AC_UL160_SR160,160_.jpg",
+    title: "The Crucible",
+    author: "Arthur Miller",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book book={book} />;
+      })}
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props.book;
   return (
     <article className="book">
       <img src={img} />
